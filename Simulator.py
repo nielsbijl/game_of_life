30e14886d16cb5dcc1e6327dc19a479df8e08a37
Niels <nbijl@petersplanet.nl>
2020-11-26 16:48:46 +0100

    Blue cell exposure test succeed

--- Simulator.py
+++ Simulator.py
@@ -1,12 +1,13 @@
 from World import *
 
+
 class Simulator:
     """
     Game of Life simulator. Handles the evolution of a Game of Life ``World``.
     Read https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life for an introduction to Conway's Game of Life.
     """
 
-    def __init__(self, world = None):
+    def __init__(self, world=None):
         """
         Constructor for Game of Life simulator.
 
@@ -25,12 +26,11 @@ class Simulator:
         :return: New state of the world.
         """
         self.generation += 1
-
-        #TODO: Do something to evolve the generation
+        # TODO: Do something to evolve the generation
         array_world = self.world.world
         for row in range(len(array_world)):
             for collum in range(len(array_world[row])):
-                if sum(self.world.get_neighbours(row, collum)) < 2:
+                if self.check_exposure(self.world.get_neighbours(row, collum)) < 2:
                     self.world.set(row, collum, 0)
 
         return self.world
@@ -59,3 +59,20 @@ class Simulator:
 
         """
         self.world = world
+
+
+
+        """    --------------------- RUELS GAME OF LIFE ---------------------      """
+
+    @staticmethod
+    def check_exposure(neighbours):
+        """
+        If a cell has less then 2 neighbours alive, the cell will die.
+
+        :return True if he dies
+                False if he stays alive
+        """
+        if sum(neighbours) < 2:
+            return True
+        else:
+            return False
